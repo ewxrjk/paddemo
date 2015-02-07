9a376c95544a602895b1443e3a241dd8d5cf79e4
Richard Kettlewell <rjk@terraraq.org.uk>
2015-02-07 12:06:54 +0000

    Move ciphertext concerns into their own class

--- paddemo/Program.cs
+++ paddemo/Program.cs
@@ -7,6 +7,35 @@ using System.Security.Cryptography;
 
 namespace paddemo
 {
+    /// <summary>
+    /// Container for a ciphertext
+    /// </summary>
+    class CipherText
+    {
+        /// <summary>
+        /// IV
+        /// </summary>
+        public byte[] iv;
+
+        /// <summary>
+        /// Encrypted bytes
+        /// </summary>
+        public byte[] encrypted;
+
+        /// <summary>
+        /// Create a ciphertext by encrypting a plaintext under a supplied key
+        /// </summary>
+        /// <param name="k">Encryption key</param>
+        /// <param name="plaintext">Plaintext to encrypt</param>
+        public CipherText(SymmetricAlgorithm k, byte[] plaintext)
+        {
+            var encryptor = k.CreateEncryptor(); // creates a fresh IV
+            iv = k.IV;
+            encrypted = encryptor.TransformFinalBlock(plaintext, 0, plaintext.Length);
+        }
+
+    }
+
     class Program
     {
 
@@ -34,23 +63,21 @@ namespace paddemo
         {
             var plaintext = "If the current Key property is null, the GenerateKey method is called to create a new random Key. If the current IV property is null, the GenerateIV method is called to create a new random IV.";
             var plaintext_bytes = System.Text.Encoding.UTF8.GetBytes(plaintext);
-            var e = k.CreateEncryptor();
-            var iv = k.IV;
-            var ciphertext_bytes = e.TransformFinalBlock(plaintext_bytes, 0, plaintext_bytes.Length);
+            var ciphertext = new CipherText(k, plaintext_bytes);
             Console.WriteLine("Original plaintext: {0}", plaintext);
-            Console.WriteLine("IV:                 {0}", ToHex(iv));
-            Console.WriteLine("Ciphertext:         {0}", ToHex(ciphertext_bytes));
+            Console.WriteLine("IV:                 {0}", ToHex(ciphertext.iv));
+            Console.WriteLine("Ciphertext:         {0}", ToHex(ciphertext.encrypted));
             var d = k.CreateDecryptor();
-            var decrypted_bytes = d.TransformFinalBlock(ciphertext_bytes, 0, ciphertext_bytes.Length);
+            var decrypted_bytes = d.TransformFinalBlock(ciphertext.encrypted, 0, ciphertext.encrypted.Length);
             var decrypted = System.Text.Encoding.UTF8.GetString(decrypted_bytes);
             Console.WriteLine("Decrypted:          {0}", decrypted);
             Console.WriteLine("");
             Console.WriteLine("Attempt to decrypt modified ciphertext...");
-            ciphertext_bytes[ciphertext_bytes.Length - 1]--;
+            ciphertext.encrypted[ciphertext.encrypted.Length - 1]--;
             d = k.CreateDecryptor();
             try
             {
-                decrypted_bytes = d.TransformFinalBlock(ciphertext_bytes, 0, ciphertext_bytes.Length);
+                decrypted_bytes = d.TransformFinalBlock(ciphertext.encrypted, 0, ciphertext.encrypted.Length);
                 decrypted = System.Text.Encoding.UTF8.GetString(decrypted_bytes);
                 Console.WriteLine("Decrypted:          {0}", decrypted);
             }
