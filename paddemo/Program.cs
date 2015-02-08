55e7a46f21ac57b4631f4ff64a6d502be421ee82
Richard Kettlewell <rjk@terraraq.org.uk>
2015-02-08 11:19:37 +0000

    Explanatory comments

--- paddemo/Program.cs
+++ paddemo/Program.cs
@@ -117,6 +117,47 @@ namespace paddemo {
             }
             // 4. replace rb by rb ⊕ i
             r[b - 1] ^= i;
+            /* What O(r|y) actually does, with D() representing a raw 1-block decryption:
+             *     (i) compute P1 = D(r) ⊕ IV
+             *    (ii) throw P1 away
+             *   (iii) compute P2 = D(y) ⊕ r
+             *    (iv) return true if P2 is correctly padded for a final block
+             *     (v) return false if it is not
+             *
+             * By now the attacker has found a value of r such that r ⊕ D(y) ends
+             * with valid padding.  In other words one of the following possibilities:
+             *      D(y) ⊕ r = ??????????????01
+             *   or D(y) ⊕ r = ????????????0202
+             *   or D(y) ⊕ r = ??????????030303
+             * etc.  (Assuming for presentational purposes that b=8.)
+             *
+             * The attacker doesn't know which of these is true so their next task
+             * is to distinguish between them somehow. The strategy used is to
+             * discover which byte has to be corrupted to break the padding.
+             *
+             * To see how this works suppose first that the situation is:
+             *      D(y) ⊕ r = 0808080808080808
+             * The attacker modifies the first byte of r:
+             *      D(y) ⊕ r ⊕ 0100000000000000 = 0908080808080808
+             * The oracle will reject this since P2 = 0908080808080808 isn't correctly padded.
+             * The attacker would therefore conclude that D(y) = 0808080808080808 ⊕ r.
+             *
+             * Suppose second that the situation is:
+             *      D(y) ⊕ r = 0507070707070707
+             * The attacker first tries modifying the first byte of r:
+             *      D(y) ⊕ r ⊕ 0100000000000000 = 0407070707070707
+             * This is still correctly padded so the oracle will accept it.  The
+             * attacker therefore moves on to the second byte of r:
+             *      D(y) ⊕ r ⊕ 0001000000000000 = 0506070707070707
+             * Now the oracle rejects this since P2 = 0506070707070707 isn't correctly padded.
+             * The attacker would therefore conclude that D(y) = ??07070707070707 ⊕ r.
+             *
+             * This exercise is repeated for each byte in turn.
+             * 
+             * The choice of ⊕ 1 as the modification of the target byte is completely
+             * arbitrary - the attacker could add 1, or invert it, or add n, anything
+             * that changes it will do.
+             */
             // 5. for n = b down to 2 do
             for (int n = b; n >= 2; --n) {
                 //  (a) take r = r1 . . . rb−n(r(b−n+1) ⊕ 1)r(b−n+2) . . . rb
@@ -131,6 +172,12 @@ namespace paddemo {
                     return results;
                 }
             }
+            /* The attacker has concluded that the padding at the end of r ⊕ D(y)
+             * is a single byte of value 1.  They can therefore recover the last
+             * byte of D(y):
+             *     D(y) ⊕ r = ??????????????01
+             *  =>     D(y) = ??????????????01 ⊕ r
+             */
             // 6. output rb ⊕ 1
             int result = r[b - 1] ^ 1;
             return new byte[] { (byte)result };
@@ -143,22 +190,44 @@ namespace paddemo {
         /// <returns>The plaintext corresponding to <paramref name="y"/></returns>
         /// <param name="prev">Array containing previous block or IV</param>
         /// <param name="offset">Offset of previous block or IV in <paramref name="prev"/></param>
+        /// <remarks>s3.2 of the paper</remarks>
         private byte[] BlockDecryptionOracle(byte[] y, byte[] prev, int offset) {
             // ry will be what the paper calls r|y
             byte[] ry = new byte[2 * b];
             // "Assuming that we already managed to get aj . . . ab for some j ≤ b ..."
-            // Note: rather than keeping a[0...b-1] around all the time, this
-            // implementation just keeps what it knows and gradually extends the
-            // array from the front (in step 5).
+            /* Note: rather than keeping a[0...b-1] around all the time, this
+             * implementation just keeps what it knows and gradually extends the
+             * array from the front (in step 5).
+             */
             byte[] a = LastWordOracle(y);
             Report(a, prev, offset);
             // r[0...b-1] will be r1...rb
             byte[] r = new byte[b];
             while (a.Length < b) {
+                /* As above, assume b=8 for presentational purposes.
+                 * 
+                 * As the loop repeats the situation looks like this:
+                 *     a = ????????????u2x1
+                 *     a = ??????????u3x2x1
+                 *     a = ????????u4x3x2x1
+                 *     a = ??????u5x4x3x2x1
+                 *     etc.
+                 * where:
+                 *    xN represents the known part of the result
+                 *    uN represents the (currently unknown) next byte to recover.
+                 *    
+                 * In the explanation below, the example values will correspond
+                 * to the half way mark:
+                 *     a = ??????u5x4x3x2x1
+                 */
                 // 2. pick r1, . . . , rj−1 at random and take i = 0
                 RNG.GetBytes(r);
                 byte i = 0;
                 // 1. take rk = ak ⊕ (b − j + 2) for k = j, . . . , b
+                /* b-j+2 is one more than the length of the known part.
+                 * So this yields something like:
+                 *    r = 00000000x4x3x2x1 ⊕ ????????05050505
+                 */
                 for (int k = 1; k <= a.Length; ++k) {
                     r[b - k] = (byte)(a[a.Length - k] ^ (a.Length + 1));
                 }
@@ -172,6 +241,27 @@ namespace paddemo {
                         break;
                     ++i;
                 }
+                /* The attack has tried inserting different bytes zz just before the known
+                 * part of a and eventually discovered:
+                 *     r = 00000000x4x3x2x1 ⊕ ??????zz05050505
+                 * ...such that O(r|y) is satisfied.  Recall that this means that
+                 * D(y) ⊕ r is correctly padded - so:
+                 *     D(y) ⊕ 00000000x4x3x2x1 ⊕ ??????zz05050505 = ??????????????01
+                 *                                          or     = ????????????0202
+                 *                                          or     = ??????????030303
+                 *                                          etc.
+                 * But the attacker also knows the trailing bytes of D(y):
+                 *     D(y) = a = ??????u5x4x3x2x1
+                 *  => ??????u5x4x3x2x1 ⊕ 00000000x4x3x2x1 ⊕ ??????zz05050505 = ??????????????01
+                 *                                                      or     = ????????????0202
+                 *                                                      or     = ??????????030303
+                 *                                                      etc.
+                 * Since the x values appear in the XOR exactly twice they can be eliminated,
+                 * and that leaves only one possible adding sequence that fits:
+                 *  => ??????u500000000 ⊕ ??????zz05050505 = ??????0505050505
+                 *  => u5 ⊕ zz = 05
+                 *  => u5 = zz ⊕ 05
+                 */
                 // 5. output r(j−1) ⊕ i ⊕ (b − j + 2)
                 byte[] anew = new byte[a.Length + 1];
                 anew[0] = (byte)(r[b - a.Length - 1] ^ i ^ (a.Length + 1));
@@ -188,6 +278,7 @@ namespace paddemo {
         /// <param name="iv">IVs</param>
         /// <param name="ys">One or more encrypted blocks</param>
         /// <returns>The plaintext corresponding to <paramref name="ys"/></returns>
+        /// <remarks>s3.3 of the paper</remarks>
         public byte[] Decrypt(byte[] iv, byte[] ys) {
             byte[] y = new byte[b];
             byte[] p = new byte[ys.Length];
